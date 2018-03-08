c6aa3e93d28376de9c63576552207dd31a72522c
Thendlessvoid <gstraub@sfsu.edu>
2018-03-07 17:43:19 -0800

    lets try again...

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -1,4 +1,4 @@
-#include "emmintrin.h"
+#include <emmintrin.h>
 /*
     Please include compiler name below (you may also include any other modules you would like to be loaded)
 
@@ -28,17 +28,17 @@ const char* dgemm_desc = "Simple blocked dgemm.";
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
 static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
 {
-  /* For each row i of A */
-  for (int i = 0; i < M; ++i)
-    /* For each column j of B */ 
-    for (int j = 0; j < N; ++j) 
-    {
-      /* Compute C(i,j) */
-      double cij = C[i+j*lda];
-      for (int k = 0; k < K; ++k)
-	    cij += A[i+k*lda] * B[k+j*lda];
-      C[i+j*lda] = cij;
-    }
+    /* For each row i of A */
+    for (int i = 0; i < M; ++i)
+        /* For each column j of B */
+        for (int j = 0; j < N; ++j)
+        {
+            /* Compute C(i,j) */
+            double cij = C[i+j*lda];
+            for (int k = 0; k < K; ++k)
+                cij += A[i+k*lda] * B[k+j*lda];
+            C[i+j*lda] = cij;
+        }
 }
 
 void do_block_fast (int lda, int M, int N, int K, double* A, double* B, double* C)
@@ -109,28 +109,28 @@ void do_block_fast (int lda, int M, int N, int K, double* A, double* B, double* 
 }/* This routine performs a dgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format. 
- * On exit, A and B maintain their input values. */  
+ * On exit, A and B maintain their input values. */
 void square_dgemm (int lda, double* A, double* B, double* C)
 {
-  /* For each block-row of A */ 
-  for (int i = 0; i < lda; i += BLOCK_SIZE)
-    /* For each block-column of B */
-    for (int j = 0; j < lda; j += BLOCK_SIZE)
-      /* Accumulate block dgemms into block of C */
-      for (int k = 0; k < lda; k += BLOCK_SIZE)
-      {
-	/* Correct block dimensions if block "goes off edge of" the matrix */
-	int M = min (BLOCK_SIZE, lda-i);
-	int N = min (BLOCK_SIZE, lda-j);
-	int K = min (BLOCK_SIZE, lda-k);
+    /* For each block-row of A */
+    for (int i = 0; i < lda; i += BLOCK_SIZE)
+        /* For each block-column of B */
+        for (int j = 0; j < lda; j += BLOCK_SIZE)
+            /* Accumulate block dgemms into block of C */
+            for (int k = 0; k < lda; k += BLOCK_SIZE)
+            {
+                /* Correct block dimensions if block "goes off edge of" the matrix */
+                int M = min (BLOCK_SIZE, lda-i);
+                int N = min (BLOCK_SIZE, lda-j);
+                int K = min (BLOCK_SIZE, lda-k);
 
-	/* Perform individual block dgemm */
-          if((M % BLOCK_SIZE == 0) && (N % BLOCK_SIZE == 0) && (K % BLOCK_SIZE == 0))
-          {
-              do_block_fast(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
-          }else{
-              do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
-          }
+                /* Perform individual block dgemm */
+                if((M % BLOCK_SIZE == 0) && (N % BLOCK_SIZE == 0) && (K % BLOCK_SIZE == 0))
+                {
+                    do_block_fast(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
+                }else{
+                    do_block(lda, M, N, K, A + i + k*lda, B + k + j*lda, C + i + j*lda);
+                }
 
-      }
+            }
 }
