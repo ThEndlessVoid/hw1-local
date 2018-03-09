eb49b16b3d7fa0273a1ef4c388dfb4b5d0b1a2cb
Thendlessvoid <gstraub@sfsu.edu>
2018-03-08 18:39:53 -0800

    unroll by 4 correctly

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -45,14 +45,14 @@ void do_block_fast (int lda, int M, int N, int K, double* A, double* B, double* 
     static double a[BLOCK_SIZE*BLOCK_SIZE] __attribute__ ((aligned (16)));
     static double a1, a2, a3, a4, b1, b2, b3, b4, c1, c2, c3, c4;
     static double temp[4] __attribute__ ((aligned (16))) = {0, 0, 0, 0};
-    __m256d vec1A;
-    __m256d vec1B;
-    __m256d vec1C;
-    __m256d vec2A;
-    __m256d vec2B;
-    __m256d vec2C;
-    __m256d vecCtmp;
-    __m256d vecCtmp2;
+//    __m256d vec1A;
+//    __m256d vec1B;
+//    __m256d vec1C;
+//    __m256d vec2A;
+//    __m256d vec2B;
+//    __m256d vec2C;
+//    __m256d vecCtmp;
+//    __m256d vecCtmp2;
     __m128d vecA1;
     __m128d vecB1;
     __m128d vecC1;
@@ -82,29 +82,29 @@ void do_block_fast (int lda, int M, int N, int K, double* A, double* B, double* 
 
             for (int k = 0; k < K; k+=4){
 
-//                vecA1 = _mm_load_pd (&a[k+i*BLOCK_SIZE]);
-//                vecA2 = _mm_load_pd (&a[(k+2)+i*BLOCK_SIZE]);
-//                vecB1 = _mm_loadu_pd (&B[k+j*lda]);
-//                vecB2 = _mm_loadu_pd (&B[(k+2)+j*lda]);
-//                vecC1 = _mm_mul_pd(vecA1, vecB1);
-//                vecC2 = _mm_mul_pd(vecA2, vecB2);
-//                vecCtmp = _mm_add_pd(vecC1, vecC2);
-//                _mm_storeu_pd(&temp[0], vecCtmp);
-//                cij += temp[0];
-//                cij += temp[1];
-
-                vec1A = _mm256_load_pd (&a[k+i*BLOCK_SIZE]);
-                vec1B = _mm256_loadu_pd (&B[k+j*lda]);
-                vec2A = _mm256_load_pd (&a[k+4+i*BLOCK_SIZE]);
-                vec2B = _mm256_loadu_pd (&B[k+4+j*lda]);
-                vec1C = _mm256_mul_pd(vec1A, vec1B);
-                vec2C = _mm256_mul_pd(vec2A, vec2B);
-                vecCtmp = _mm256_add_pd(vec1C,vec2C);
-                _mm256_store_pd(&temp[0], vecCtmp);
+                vecA1 = _mm_load_pd (&a[k+i*BLOCK_SIZE]);
+                vecA2 = _mm_load_pd (&a[(k+2)+i*BLOCK_SIZE]);
+                vecB1 = _mm_loadu_pd (&B[k+j*lda]);
+                vecB2 = _mm_loadu_pd (&B[(k+2)+j*lda]);
+                vecC1 = _mm_mul_pd(vecA1, vecB1);
+                vecC2 = _mm_mul_pd(vecA2, vecB2);
+                vecCtmp = _mm_add_pd(vecC1, vecC2);
+                _mm_storeu_pd(&temp[0], vecCtmp);
                 cij += temp[0];
                 cij += temp[1];
-                cij += temp[2];
-                cij += temp[3];
+
+//                vec1A = _mm256_load_pd (&a[k+i*BLOCK_SIZE]);
+//                vec1B = _mm256_loadu_pd (&B[k+j*lda]);
+//                vec2A = _mm256_load_pd (&a[k+4+i*BLOCK_SIZE]);
+//                vec2B = _mm256_loadu_pd (&B[k+4+j*lda]);
+//                vec1C = _mm256_mul_pd(vec1A, vec1B);
+//                vec2C = _mm256_mul_pd(vec2A, vec2B);
+//                vecCtmp = _mm256_add_pd(vec1C,vec2C);
+//                _mm256_store_pd(&temp[0], vecCtmp);
+//                cij += temp[0];
+//                cij += temp[1];
+//                cij += temp[2];
+//                cij += temp[3];
 
             }
             C[i+j*lda] = cij;
